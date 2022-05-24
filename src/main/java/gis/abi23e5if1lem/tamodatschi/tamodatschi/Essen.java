a2bcd9b233c534337d7c54f6b8c06d898f4a3a70
Noah Siebart <nosiebart@gmail.com>
2022-05-24 09:14:38 +0200

    Updated all Files

---
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Essen.java
@@ -0,0 +1,15 @@
+package gis.abi23e5if1lem.tamodatschi.tamodatschi;
+
+public class Essen {
+
+    private String name;
+    private int[] buffs;
+
+    public Essen() {
+
+    }
+
+    public void konsumieren(){
+
+    }
+}
---
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Shop.java
@@ -0,0 +1,35 @@
+package gis.abi23e5if1lem.tamodatschi.tamodatschi;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Shop extends Application {
+
+    private List<Essen> inventar = new ArrayList();
+
+    public Shop(){
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    private int[] lootbox1(){
+        return null;
+    }
+
+    private int[] lootbox2(){
+        return null;
+    }
+
+    private int[] lootbox3(){
+        return null;
+    }
+
+
+}
