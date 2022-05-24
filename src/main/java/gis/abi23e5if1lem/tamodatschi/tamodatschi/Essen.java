96138114b578b4ba297f3df0fb08ad2b7587afe3
Dabor2412 <95739088+Dabor2412@users.noreply.github.com>
2022-05-24 19:44:19 +0200

    Create Essen.java

---
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Essen.java
@@ -0,0 +1,14 @@
+package gis.abi23e5if1lem.tamodatschi.tamodatschi;
+
+public class Essen {
+    private String name;
+    private int[] buffs;
+
+    public Essen(String name, int[] buffs) {
+        this.name = name;
+        this.buffs = buffs;
+    }
+    public void konsumieren() {
+
+    }
+}
