f8a0cf0b0b1b1252d03375d3f0bfa0fae0cf71e7
Parkyri <105343168+Parkyri@users.noreply.github.com>
2022-06-03 13:05:08 +0200

    Memory falsch

--- src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
@@ -25,13 +25,6 @@ public class Memory {
         stage.setScene(scene);
         stage.show();
 
-        ImageView apfel = new ImageView(new Image(getClass().getResource("images/apple.jpg").toString()));
-        apfel.setFitHeight(45);
-        apfel.setFitWidth(45);
-
-        ImageView orange = new ImageView(new Image(getClass().getResource("images/Orange.jpg").toString()));
-        orange.setFitWidth(45);
-        orange.setFitHeight(45);
 
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
@@ -42,16 +35,86 @@ public class Memory {
 
                 root.getChildren().add(b);
                 buttons[j][i] = b;
-
-                b.setOnAction(e -> {
-                    Random rd = new Random();
-                    if (rd.nextInt(100) >= 50) {
-                        b.setGraphic(apfel);
-                    } else {
-                        b.setGraphic(orange);
-                    }
-                });
+                zuweisung(b);
             }
         }
+
     }
-}
+
+    public void zuweisung(Button b){
+        Random rd = new Random();
+
+        ImageView apfel = new ImageView(new Image(getClass().getResource("images/apple.jpg").toString()));
+        apfel.setFitHeight(45);
+        apfel.setFitWidth(45);
+        int apple = 0;
+
+        ImageView orange = new ImageView(new Image(getClass().getResource("images/Orange.jpg").toString()));
+        orange.setFitWidth(45);
+        orange.setFitHeight(45);
+        int orang = 0;
+
+        ImageView melone = new ImageView(new Image(getClass().getResource("images/Melone.jpg").toString()));
+        melone.setFitWidth(45);
+        melone.setFitHeight(45);
+        int melon = 0;
+
+        ImageView paprika = new ImageView(new Image(getClass().getResource("images/paprika.jpg").toString()));
+        paprika.setFitWidth(45);
+        paprika.setFitHeight(45);
+        int paprik = 0;
+
+        ImageView gurke = new ImageView(new Image(getClass().getResource("images/Gurke.jpg").toString()));
+        gurke.setFitWidth(45);
+        gurke.setFitHeight(45);
+        int cucumber = 0;
+
+        ImageView birne = new ImageView(new Image(getClass().getResource("images/Birne.jpg").toString()));
+        birne.setFitWidth(45);
+        birne.setFitHeight(45);
+        int birn = 0;
+
+        ImageView banane = new ImageView(new Image(getClass().getResource("images/Banane.jpg").toString()));
+        banane.setFitWidth(45);
+        banane.setFitHeight(45);
+        int banana = 0;
+
+        ImageView tomate = new ImageView(new Image(getClass().getResource("images/Tomate.jpg").toString()));
+        tomate.setFitWidth(45);
+        tomate.setFitHeight(45);
+        int tomato = 0;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (rd.nextInt(16) <= 8 && apple < 2){
+                   b.setGraphic(apfel);
+                    apple ++;
+                } else if (rd.nextInt(16) <= 8 && orang < 2){
+                    b.setGraphic(orange);
+                    orang ++;
+                }  else if (rd.nextInt(16) <= 8 && melon < 2){
+                    b.setGraphic(melone);
+                    melon ++;
+                } else if (rd.nextInt(16) <= 8 && paprik < 2){
+                    b.setGraphic(paprika);
+                    paprik ++;
+                }  else if (rd.nextInt(16) >  8 && cucumber < 2){
+                    b.setGraphic(gurke);
+                    cucumber ++;
+                }  else if (rd.nextInt(16) >  8 && birn < 2){
+                    b.setGraphic(birne);
+                    birn ++;
+                }  else if (rd.nextInt(16) >  8 && banana < 2){
+                    b.setGraphic(banane);
+                    banana ++;
+                }  else if (rd.nextInt(16) >  8 && tomato < 2){
+                    b.setGraphic(tomate);
+                    tomato ++;
+                }
+                System.out.println(rd.nextInt(16));
+
+            }
+        }
+
+       }
+    }
