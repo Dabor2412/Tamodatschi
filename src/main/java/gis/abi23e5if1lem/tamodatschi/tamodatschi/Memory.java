cb201a4b1e20f7e6e8b0c2aa45429184d49d6318
Parkyri <105343168+Parkyri@users.noreply.github.com>
2022-06-03 21:27:53 +0200

    Update Memory.java

--- src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
@@ -15,6 +15,9 @@ public class Memory {
 
     int Zaehler = 0;
     private Button buttons[][] = new Button[4][4];
+    List<String> list = new ArrayList<>();
+
+
 
     public void start() {
         Pane root = new Pane();
@@ -26,6 +29,29 @@ public class Memory {
         stage.show();
 
 
+
+        list.add("Apfel");
+        list.add("Apfel");
+        list.add("Orange");
+        list.add("Orange");
+        list.add("Melone");
+        list.add("Melone");
+        list.add("Paprika");
+        list.add("Paprika");
+        list.add("Gurke");
+        list.add("Gurke");
+        list.add("Birne");
+        list.add("Birne");
+        list.add("Banane");
+        list.add("Banane");
+        list.add("Tomate");
+        list.add("Tomate");
+        System.out.println(list);
+
+        Collections.shuffle(list);
+
+        System.out.println(list);
+
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 Button b = new Button();
@@ -35,86 +61,79 @@ public class Memory {
 
                 root.getChildren().add(b);
                 buttons[j][i] = b;
-                zuweisung(b);
+                b.setOnAction( e ->
+                zuweisung(b)
+                );
             }
+
         }
 
     }
 
     public void zuweisung(Button b){
-        Random rd = new Random();
 
         ImageView apfel = new ImageView(new Image(getClass().getResource("images/apple.jpg").toString()));
         apfel.setFitHeight(45);
         apfel.setFitWidth(45);
-        int apple = 0;
-
         ImageView orange = new ImageView(new Image(getClass().getResource("images/Orange.jpg").toString()));
         orange.setFitWidth(45);
         orange.setFitHeight(45);
-        int orang = 0;
-
         ImageView melone = new ImageView(new Image(getClass().getResource("images/Melone.jpg").toString()));
         melone.setFitWidth(45);
         melone.setFitHeight(45);
-        int melon = 0;
-
         ImageView paprika = new ImageView(new Image(getClass().getResource("images/paprika.jpg").toString()));
         paprika.setFitWidth(45);
         paprika.setFitHeight(45);
-        int paprik = 0;
-
         ImageView gurke = new ImageView(new Image(getClass().getResource("images/Gurke.jpg").toString()));
         gurke.setFitWidth(45);
         gurke.setFitHeight(45);
-        int cucumber = 0;
-
         ImageView birne = new ImageView(new Image(getClass().getResource("images/Birne.jpg").toString()));
         birne.setFitWidth(45);
         birne.setFitHeight(45);
-        int birn = 0;
-
         ImageView banane = new ImageView(new Image(getClass().getResource("images/Banane.jpg").toString()));
         banane.setFitWidth(45);
         banane.setFitHeight(45);
-        int banana = 0;
-
         ImageView tomate = new ImageView(new Image(getClass().getResource("images/Tomate.jpg").toString()));
         tomate.setFitWidth(45);
         tomate.setFitHeight(45);
-        int tomato = 0;
 
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                if (rd.nextInt(16) <= 8 && apple < 2){
-                   b.setGraphic(apfel);
-                    apple ++;
-                } else if (rd.nextInt(16) <= 8 && orang < 2){
-                    b.setGraphic(orange);
-                    orang ++;
-                }  else if (rd.nextInt(16) <= 8 && melon < 2){
-                    b.setGraphic(melone);
-                    melon ++;
-                } else if (rd.nextInt(16) <= 8 && paprik < 2){
-                    b.setGraphic(paprika);
-                    paprik ++;
-                }  else if (rd.nextInt(16) >  8 && cucumber < 2){
-                    b.setGraphic(gurke);
-                    cucumber ++;
-                }  else if (rd.nextInt(16) >  8 && birn < 2){
-                    b.setGraphic(birne);
-                    birn ++;
-                }  else if (rd.nextInt(16) >  8 && banana < 2){
-                    b.setGraphic(banane);
-                    banana ++;
-                }  else if (rd.nextInt(16) >  8 && tomato < 2){
-                    b.setGraphic(tomate);
-                    tomato ++;
-                }
-                System.out.println(rd.nextInt(16));
-
+        for (int k = 0; k < 1; k++) {
+            if (list.get(k).equals("Apfel")) {
+                b.setGraphic(apfel);
+                b.setDisable(true);
+                list.remove("Apfel");
+            } else if (list.get(k).equals("Orange")) {
+                b.setGraphic(orange);
+                b.setDisable(true);
+                list.remove("Orange");
+            } else if (list.get(k).equals("Melone")) {
+                b.setGraphic(melone);
+                b.setDisable(true);
+                list.remove("Melone");
+            } else if (list.get(k).equals("Paprika")) {
+                b.setGraphic(paprika);
+                b.setDisable(true);
+                list.remove("Paprika");
+            } else if (list.get(k).equals("Gurke")) {
+                b.setGraphic(gurke);
+                list.remove("Gurke");
+            } else if (list.get(k).equals("Birne")) {
+                b.setGraphic(birne);
+                b.setDisable(true);
+                list.remove("Birne");
+            } else if (list.get(k).equals("Banane")) {
+                b.setGraphic(banane);
+                b.setDisable(true);
+                list.remove("Banane");
+            } else if (list.get(k).equals("Tomate")) {
+                b.setGraphic(tomate);
+                b.setDisable(true);
+                list.remove("Tomate");
             }
         }
+       }
+
+       public void ueberpruefung(Button b){
 
        }
     }
