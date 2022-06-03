3c47f78cf774421be24b15635859a0be0a1c654c
Parkyri <105343168+Parkyri@users.noreply.github.com>
2022-06-03 11:32:15 +0200

    Memory ist Scheiße
    Mach es am Wochenende fertig

--- src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Memory.java
@@ -1,49 +1,57 @@
 package gis.abi23e5if1lem.tamodatschi.tamodatschi;
 
-import javafx.application.Application;
-import javafx.geometry.HPos;
-import javafx.geometry.Pos;
-import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-import java.util.Random;
+import java.util.*;
 
 
 public class Memory {
 
+    int Zaehler = 0;
+    private Button buttons[][] = new Button[4][4];
+
     public void start() {
         Pane root = new Pane();
 
-        Scene scene = new Scene(root, 800, 500);
+        Scene scene = new Scene(root, 800, 800);
         Stage stage = new Stage();
         stage.setTitle("Memory");
         stage.setScene(scene);
         stage.show();
 
-        for(int i = 0; i < 4;i++ ) {
+        ImageView apfel = new ImageView(new Image(getClass().getResource("images/apple.jpg").toString()));
+        apfel.setFitHeight(45);
+        apfel.setFitWidth(45);
+
+        ImageView orange = new ImageView(new Image(getClass().getResource("images/Orange.jpg").toString()));
+        orange.setFitWidth(45);
+        orange.setFitHeight(45);
+
+        for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
                 Button b = new Button();
                 b.setLayoutX(i * 65 + 270);
-                b.setLayoutY(j * 65 + 120);
-                b.setPrefSize(60,60);
+                b.setLayoutY(j * 65 + 270);
+                b.setPrefSize(60, 60);
+
                 root.getChildren().add(b);
+                buttons[j][i] = b;
 
-
+                b.setOnAction(e -> {
+                    Random rd = new Random();
+                    if (rd.nextInt(100) >= 50) {
+                        b.setGraphic(apfel);
+                    } else {
+                        b.setGraphic(orange);
+                    }
+                });
             }
         }
-
-    }
-
-    public void ueberpruefung(){
-
     }
 }
