f1ec636b2f100908be523aa0cca9880273ff15f4
a2b2c2d2 <98219862+a2b2c2d2@users.noreply.github.com>
2022-06-17 10:06:03 +0200

    Update Minigame.java

--- src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Minigame.java
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Minigame.java
@@ -1,7 +1,7 @@
 package gis.abi23e5if1lem.tamodatschi.tamodatschi;
-
+// Minigame ist eine Unterklasse von Ort
 public class Minigame extends Ort {
-
+    //es wird festgelegt welches Minispiel ausgeführt wird
     public int minigameId = 0;
     public void start() {
         switch (minigameId){
@@ -13,18 +13,14 @@ public class Minigame extends Ort {
                 Memory mem = new Memory();
                 mem.start(1);
                 break;
-            case 2:
-                NoelSpricht nsp = new NoelSpricht();
-                nsp.start(1);
-                break;
         }
     }
-
+    //Konstruktor0 
     public Minigame(int x, int y, int minigameId){
         super(x, y);
         this.minigameId = minigameId;
     }
-
+    //Konstrucktor1
     public Minigame(int x, int y, String grafik, int minigameId){
         super(x, y, grafik);
         this.minigameId = minigameId;
