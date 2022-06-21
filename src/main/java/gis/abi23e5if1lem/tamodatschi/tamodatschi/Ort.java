6aa955f17e080cee5327545c4d8ee928b6a6dded
a2b2c2d2 <98219862+a2b2c2d2@users.noreply.github.com>
2022-06-21 08:11:29 +0200

    Update Ort.java

--- src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Ort.java
+++ src/main/java/gis/abi23e5if1lem/tamodatschi/tamodatschi/Ort.java
@@ -1,5 +1,5 @@
 package gis.abi23e5if1lem.tamodatschi.tamodatschi;
-
+//Klasse mit den Attributen positionX, positionY und grafik, mit ensprechenden Setter- und Gettermethoden
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 //Klasse mit den Attributen position x  und y sowie Graphik, dient als Superklasse für Minigame, Shop und vieleicht Bossfight. Entsprechende Setter und Getter-Methoden sind vorhanden. Es gibt zwei Konstruktoren. 
@@ -31,12 +31,12 @@ public class Ort {
     public void setPositionY(int positionY) {
         this.positionY = positionY;
     }
-
+    //Konstruktor für den Fall, dass die Grafik in der Klasse Spielfeld festgellt wird
     public Ort(int x, int y) {
         this.positionX = x;
         this.positionY = y;
     }
-
+    //Konstrucktor  bei dem die Grafik mit festgelegt wird
     public Ort(int x, int y, String grafik) {
         this.positionX = x;
         this.positionY = y;
